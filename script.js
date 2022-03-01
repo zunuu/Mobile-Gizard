5fcfb7f1c2cc276d4135f7daf38149ac4fcfc5ba
zunuu <zunayedhossain99@gmail.com>
2022-03-02 04:05:24 +0600

    API Phone Search Done

--- script.js
+++ script.js
@@ -26,5 +26,43 @@ const searchMobile = () => {
 }
 const displayMobile = allPhones => {
 
+    // for (const eachPhone of allPhones) {
+    //     
+    // }
+    const allMobiles = document.getElementById('allMobiles')
+    allPhones.forEach(eachPhone => {
+        // console.log(eachPhone.brand)
+        // console.log(eachPhone.phone_name)
+        // console.log(eachPhone.image)
+        console.log(eachPhone)
+        const newMobile = document.createElement('div');
+
+        newMobile.innerHTML = `
+        
+        <div class="border border-5 border-white mx-auto bg-transparent rounded-3 shadow-lg" style="width: 10rem;">
+            <img src="${eachPhone.image}" class="card-img-top rounded-3 w-100" alt="...">
+            
+            <ul class="list-group bg-transparent rounded-3 list-group-flush">
+                <li class="list-group-item rounded-3 bg-info bg-gradient bg-opacity-25 text-dark">${eachPhone.brand}</li>
+                <li class="list-group-item rounded-3 bg-danger bg-gradient bg-opacity-25 text-dark"><strong>${eachPhone.phone_name}</strong></li>
+                <li class="list-group-item bg-white p-0 pt-1 bg-gradient   bg-opacity-25 text-dark">
+                    
+                        <button type="button" class="px-4 rounded-pill btn btn-primary" data-bs-toggle="modal"  data-bs-target="#exampleModal">
+                           
+                              See Details
+                           
+                        </button>
+                  
+                </li>
+            </ul>
+           
+        </div>
+       
+        `
+        newMobile.classList.add("col-xl-4", "p-5", "my-5", "bg-transparent", "text-center")
+        allMobiles.appendChild(newMobile);
+    })
+
+
 
 }
